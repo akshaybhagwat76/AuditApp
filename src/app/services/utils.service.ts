3e2e2d5b2e0722db9aba9c5d1a92438fe4233aeb
akshaybhagwat76 <akshaybhagwat76@gmail.com>
2022-07-05 16:52:57 +0530

    Update utils.service.ts

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,20 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { SERVER_API_URL } from '../app.constant';
+import { SERVER_API_URL } from './app.constant';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class UtilsService {
-  constructor(private http: HttpClient) {}
 
+
+  constructor(private http: HttpClient) { }
+
+  // Get User List API Using GET Method:
   getRole() {
-    return this.http.get(SERVER_API_URL + '/dev/users/audit');
+    return this.http.get(SERVER_API_URL + '/dev/users/audit')
   }
 
+  // Create User API Using POST Method:
   createUser(requestForm: any) {
-    return this.http.post(SERVER_API_URL + '/dev/users/audit', requestForm, {
-      responseType: 'text',
-    });
+    return this.http.post(SERVER_API_URL + '/dev/users/audit', requestForm, { responseType: 'text' })
   }
 }
