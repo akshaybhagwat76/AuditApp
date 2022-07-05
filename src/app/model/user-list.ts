d9c85e6e714d1bfcf3e69fa27baef70c79669f60
akshaybhagwat76 <akshaybhagwat76@gmail.com>
2022-07-05 16:52:33 +0530

    Update user-list.ts

--- src/app/model/user-list.ts
+++ src/app/model/user-list.ts
@@ -1,15 +1,14 @@
+// User Object
 export interface UserList {
-  user: [
-    {
-      assignments: [];
-      company: string;
-      first_name: string;
-      id: number;
-      last_name: string;
-      phone: string;
-      position_emp: string;
-      roles: [];
-      user_name: string;
-    }
-  ];
+    user: [{
+        assignments: [],
+        company: string,
+        first_name: string,
+        id: number,
+        last_name: string,
+        phone: string,
+        position_emp: string,
+        roles: []
+        user_name: string,
+    }]
 }
